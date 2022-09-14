143fb2b829f741e85822edd01efb016c596ebc48
BettyMwiti <bettykawiram@gmail.com>
2022-09-14 10:54:11 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,1 +1,42 @@
+#include "main.h"                                                                                                                 
+
+                                                                                                                                  
+
+/**                                                                                                                               
+
+  * jack_bauer - Prints every minute of the day of Jack Bauer                                                                     
+
+  *                                                                                                                               
+
+  * Return: ...                                                                                                                   
+
+  */                                                                                                                              
+
+                                                                                                                                  
+
+void jack_bauer(void)                                                                                                             
+
+                                                                                                                                  
+
+{                                                                                                                                 
+
+                                                                                                                                  
+
+        int a, b, c, d;                                                                                                           
+
+                                                                                                                                  
+
+                                                                                                                                  
+
+                                                                                                                                  
+
+        for (a = 0; a <= 2; a++)                                                                                                  
+
+                                                                                                                                  
+
+        {                                                                                                                         
+
+                                                                                                                                  
+
+                for (b = 0; b <= 9; b++)                                                                                          
 
