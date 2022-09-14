4ff8439388f6803b0c81a720a23002e151014626
BettyMwiti <bettykawiram@gmail.com>
2022-09-14 06:07:19 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * print_alphabet - function to print abc
+  *
+  * Return: void
+  */
+void print_alphabet(void)
+{
+	char c;
+
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
+
