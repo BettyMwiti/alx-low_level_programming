a2811e588324429745c424995943b35f5d0ceb75
BettyMwiti <bettykawiram@gmail.com>
2022-09-14 05:39:37 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-  * main - Entry point
-  *
+  * main - print putchar
   * Return: Always 0
   */
+
 int main(void)
 {
 	_putchar('_');
@@ -18,4 +18,5 @@ int main(void)
 	_putchar('\n');
 
 	return (0);
+
 }
