c43b82fd701d3d73bc57fef20a0691bc859c3e9e
BettyMwiti <bettykawiram@gmail.com>
2022-09-14 05:27:34 -0700

    Task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,6 @@
 #ifndef main.h
 #define main.h
 
-/*
- * File: main.h
- * Auth: BettyMwiti
- * Desc: Header file containing declarations for all functions
- *       used in the 0x02-functions_nested_loops directory.
- */
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
